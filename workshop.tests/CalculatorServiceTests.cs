c68ea2e259c064311a44a9a93ebc752bceab665b
uerbzr <nigel.sibbert@gmail.com>
2025-08-19 07:53:46 +0100

    Created service, tests

---
+++ workshop.tests/CalculatorServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using workshop.calculator;
+
+namespace workshop.tests;
+
+public class CalculatorServiceTests
+{
+    
+
+    [TestCase(1,2,3)]
+    public void AddTests(int a, int b, int expectedResult)
+    {
+        //arrange
+        CalculatorService service = new CalculatorService();
+
+        //act
+        int result = service.Add(a, b);
+
+        //assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+
+
+
+    }
+}
